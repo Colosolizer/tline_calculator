72e9f4d548782583fb173706b3b4d24908f75f20
Colosolizer <sekouruddock@gmail.com>
2024-10-17 15:49:42 -0500

    made new file and code adjustments

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_xlider/flutter_xlider.dart';
+//import 'package:flutter_titled_container/flutter_titled_container.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
