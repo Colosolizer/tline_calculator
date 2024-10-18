5c3b7b179e11c156a87eae7eda1668df3d947351
Colosolizer <sekouruddock@gmail.com>
2024-10-17 23:51:33 -0500

    added a button

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tline_calculator/utils/app_styles.dart';
 //import 'package:flutter_xlider/flutter_xlider.dart';
 //import 'package:flutter_titled_container/flutter_titled_container.dart';
 
@@ -12,6 +13,76 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Apptheme.dark,
+      body: Column(
+        //Main Body Column That Holds Everything Together
+        children: [
+          SizedBox(
+              width: 1000,
+              height:
+                  30), //Sizedbox creates space between the top of the app and the container
+          Row(
+            //Top Row of  Containers with Graph, Diagram and Outputs
+            children: [
+              SizedBox(width: 30, height: 5),
+              SizedBox(
+                //Contains the First Container
+                width: 400,
+                height: 400,
+                child:
+                    Container(width: 500, height: 500, color: Apptheme.darker),
+              ), //Box one with container 1
+              SizedBox(width: 30, height: 5),
+              SizedBox(
+                //Contains the First Container
+                width: 400,
+                height: 400,
+                child:
+                    Container(width: 500, height: 500, color: Apptheme.darker),
+              ), //Box one with container 2
+              SizedBox(width: 30, height: 5),
+              SizedBox(
+                //Contains the First Container
+                width: 400,
+                height: 400,
+                child:
+                    Container(width: 500, height: 500, color: Apptheme.darker),
+              ) //Box one with container 3
+            ],
+          ), //END OF TOP ROW
+
+          SizedBox(width: 1000, height: 30),
+
+          //ROW 2 WITH SLIDERS
+          Row(
+              mainAxisAlignment: MainAxisAlignment.center, // Center the button
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(
+                      8.0), // Add padding around the button
+                  child: _mainbutton(() {
+                    // Define what happens when the button is pressed
+                    print('Button1 pressed');
+                  }, 'Button1'),
+                ),
+              ])
+        ],
+      ),
+    );
+  }
+
+  //A button method that can be reused multiple times
+  ElevatedButton _mainbutton(Function()? onPressed, String text) {
+    return ElevatedButton(
+        onPressed: onPressed, style: _buttonStyle(), child: Text(text));
+  }
+
+  ButtonStyle _buttonStyle() {
+    return ElevatedButton.styleFrom(
+      backgroundColor: Apptheme.accent,
+      foregroundColor: Apptheme.darker,
+      elevation: 5.0,
+    );
   }
 }
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+  static const Color dark = Color(0xFF242203);
+  static const Color darker = Color(0xFF151402);
+  static const Color accent = Color(0xFFE9DF2E);
+}
